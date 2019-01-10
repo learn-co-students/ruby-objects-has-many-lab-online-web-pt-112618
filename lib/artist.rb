da02f56b2e917693bfe97b6f41b64b52688cff0f
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-10 20:11:11 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+  
+ attr_accessor :name
+ 
+ @@all = []
+ 
+ def initialize(name)
+   @name = name
+   @songs = []
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
+ def songs
+   @songs
+ end
+ 
+ def add_song(song)
+   @songs << song
+   song.artist = self
+ end
+
+ def add_song_by_name(song)
+   song = Song.new(song)
+   @songs << song
+   song.artist = self
+ end
+ 
+ def self.song_count
+   Song.all.count
+ end
+end
